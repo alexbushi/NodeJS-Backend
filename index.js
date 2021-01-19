d8b1e3a3899e13d67ad47cf665bb99cfb825abca
alexbushi <alexbush@udel.edu>
2021-01-19 16:30:42 -0500

    initial commit with express server setup

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send([1,2,3,4]);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
