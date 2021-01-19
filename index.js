a6e3d388643430e52fb8d2e473d57fcd02f9274f
alexbushi <alexbush@udel.edu>
2021-01-19 17:49:32 -0500

    added configuration and environments and debugging

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+const debug = require('debug')('app:startup')
+// need to run DEBUG=app:startup nodemon index.js
+
 const express = require('express');
 const config = require('config');
 const helmet = require('helmet');
 const logger = require('./logger');
 const app = express();
 
+// export NODE_ENV=development
+// if did export NODE_ENV=default config.get will pull from respective file
 if (app.get('env') === 'development') {
     // load some other middleware, etc...
 }
 
-// Configuration
-console.log('Application Name: ' + config.get('name'));
-//console.log('Password: ' + config.get('password'))
+debug('Application Name: ' + config.get('name'));
+// need to say export app_password=1234 or whatever we neeed in custom-environment-variables file
+debug('Password: ' + config.get('password'))
 
+// Middleware = .use
 app.use(helmet());
 
 app.use(logger);
