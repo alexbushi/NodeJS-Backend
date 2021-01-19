a1c6caaa3146752d627b9c50137b3a515edc3e9d
alexbushi <alexbush@udel.edu>
2021-01-19 17:07:14 -0500

    added middleware file notes

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const helmet = require('helmet');
+const logger = require('./logger');
 const app = express();
 
+app.use(helmet());
+
+app.use(logger);
+
 app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+app.get('/api/courses', (req, res) => {
     res.send([1,2,3,4]);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+    //req.query returns object of the query parameters
 });
 
 const port = process.env.PORT || 5000;
